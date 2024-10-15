c0b956ce59774a1fdc7e307013e96038bfb4e2fb
jvmgomes <joao@mgomes.net>
2024-10-15 18:17:48 -0300

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,10 @@
+numero1 = input()
+numero2 = input()
+numero1_int = int(numero1)
+numero2_int = int(numero2)
+
+quociente = numero1_int / numero2_int
+quociente = int(quociente)
+resto = numero1_int % numero2_int
+soma = quociente + resto
+print(soma) 
